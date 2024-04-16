24d68714e52f2452bf674f5be00273a0da0b798f
prlakshm <pranavi_lakshminarayanan@brown.edu>
2024-04-16 13:44:17 -0400

    Feat: edit button UI changed

--- meyers-lab/app/data/page.tsx
+++ meyers-lab/app/data/page.tsx
@@ -511,7 +511,7 @@ export default function Data() {
                 className="button-overlay focus:shadow-outline font-regular rounded border border-gray-400 bg-gray-200 px-4 py-2 text-gray-800 hover:border-gray-500 hover:bg-gray-100 focus:outline-none"
                 onClick={onEditClick}
               >
-                Edit
+                <img src='./edit_icon.png' alt="Edit Icon"/>
               </button>
             </div>
           </div>
