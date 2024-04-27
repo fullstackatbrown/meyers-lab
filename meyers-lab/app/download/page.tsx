d4aec4a8ab8f9a592b2a5142cfc67f3b2439189a
Alan W <awangc31s@gmail.com>
2024-04-27 15:45:55 -0500

    text size change

--- meyers-lab/app/download/page.tsx
+++ meyers-lab/app/download/page.tsx
@@ -18,7 +18,7 @@ function FormElt({ label, name, type }: { label: string, name: string, type: str
     }
     return (
         <div className="p-2 w-full">
-            <div className="text-left">{label}</div>
+            <div className="text-left text-lg">{label}</div>
             {inputElt}
         </div>
     );
@@ -36,46 +36,18 @@ function Form() {
         .catch(() => setIsverified(false))
     }
 
-<<<<<<< HEAD
     const scriptUrl = "https://script.google.com/macros/s/AKfycbzCxfzdO0JkMVIjt3-ynby9SHyQfyALYgLFgo2rXA2AHA_Y3Cck99nJUgrFrgUBqguA/exec"
     const [formdone, setFormdone] = useState(false);
 
-    const redirect = () => {
-        //setFormdone(true);
-    }
-
-    const submitBtn = isVerified ? <button id="submit" type="submit" value="Submit" onClick={redirect}
+    const submitBtn = isVerified ? <button id="submit" type="submit" value="Submit"
     className="w-32 border-black bg-rose-500 rounded-3xl text-white h-10 mt-5 text-lg">Submit</button> : <input id="submit" type="button" value="Submit"
     className="w-32 border-black bg-rose-500 rounded-3xl text-white h-10 mt-5 text-lg"/>
 
-    return formdone ? 
-    <div>
-        <div className="text-3xl"></div>
-    </div> : (
+    return (
         <form id="data-form" action={scriptUrl} target="_blank" method="POST" className="w-1/2 min-w-[340px] text-left">
             <div id="explanation" className="mb-8 text-lg">
                 Fill out the form to request a download link.
             </div>
-=======
-    const scriptUrl = "https://script.google.com/macros/s/AKfycbwidPhci5rH5D-_gm-0dB-OR6U_1dqDc0UTH99Pl3sxZL3pkK7_iwX50ltwXvTmHL75/exec"
-    const [formdone, setFormdone] = useState(false);
-
-    const redirect = () => {
-        setFormdone(true);
-    }
-
-    const submitBtn = isVerified ? <input id="submit" type="submit" value="Submit"
-    className="w-32 border-black bg-primary-red rounded-xl text-white h-10 mt-5 text-lg font-circ-std "/> : <input id="submit" type="button" value="Submit"
-    className="w-32 border-black bg-primary-red rounded-xl text-white h-10 mt-5 text-lg font-circ-std "/>
-
-    return formdone ? 
-    <div>
-        <div className="text-3xl">Your download has started...</div>
-        <div className="mt-3"><a className="text-blue underline p-1" href="https://docs.google.com/spreadsheets/d/e/2PACX-1vQeRBpaGQcuxhPaCIi_T4aUe5bigaiSq4z0RkOkIJmEFLjYPp85U_MCvqNAWx-EPCbJetNvTPOvYNiJ/pub?output=xlsx">Click here to download manually</a></div>
-        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQeRBpaGQcuxhPaCIi_T4aUe5bigaiSq4z0RkOkIJmEFLjYPp85U_MCvqNAWx-EPCbJetNvTPOvYNiJ/pub?output=xlsx"></iframe>
-    </div> : (
-        <form id="data-form" action={scriptUrl} target="dummyframe" method="POST" className="w-1/2 text-left" onSubmit={redirect}>
->>>>>>> 3a3b5b0567f0e49ca6f3055301e355d489581cc9
             <div className="flex text-center">   
                 <FormElt label="First Name: " name="first-name" type="text" />
                 <FormElt label="Last Name: " name="last-name" type="text" />
@@ -130,18 +102,11 @@ export default function Download() {
 
         {/* Site content */}
         <div id="content" className="text-center p-5">
-            <div id="title-text" className="mt-10 mb-4 text-4.5xl-responsive font-bold font-circ-std text-primary">
+            <div id="title-text" className="mt-1 mb-4 text-4.5xl-responsive font-bold text-primary">
                 Data Download
-            </div>
-<<<<<<< HEAD
-            <div className="flex justify-center">
-=======
-            <div id="explanation" className="mb-8 text-lg">
-                Fill out the form to request a download link.
             </div>
             <div className="flex justify-center">
             <iframe name="dummyframe" id="dummyframe" className="hidden"></iframe>
->>>>>>> 3a3b5b0567f0e49ca6f3055301e355d489581cc9
             <Form/>
             </div>
         </div>
