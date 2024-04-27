c0d7c212a5e0be71f14fe14eccdfbcf0a3f797a8
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2024-04-27 16:54:40 -0400

    add placeholder

--- meyers-lab/app/page.tsx
+++ meyers-lab/app/page.tsx
@@ -85,10 +85,10 @@ export default function Page() {
       </div>
 
       {/* Instructions section */}
-      <div className="mx-24 my-[5vh] min-h-[10vh] p-4 text-2xl font-bold">
+      <div className="mx-24 my-[5vh] min-h-[10vh] p-4  w-1/2 text-2xl font-bold">
         <h1> Instructions </h1>
         <div className="my-[5vh] min-h-[10vh] text-xl font-normal">
-          <h1></h1>
+          <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h1>
         </div>
       </div>
 
