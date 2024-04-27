b71fad450c318139c00bfe7d113d731db4e33794
prlakshm <pranavi_lakshminarayanan@brown.edu>
2024-04-27 17:08:40 -0400

    Fix: image height

--- meyers-lab/app/page.tsx
+++ meyers-lab/app/page.tsx
@@ -27,9 +27,9 @@ export default function Page() {
           <img
             src="./brown-campus.png"
             alt="Brown University"
-            style={{ height: '55vh', width: '100%', objectFit: 'cover' }}
+            style={{ height: '57vh', width: '100%', objectFit: 'cover' }}
           />
-          <div className="mt-[-16vh] p-4 text-center text-4xl font-bold text-white">
+          <div className="mt-[-15vh] p-4 text-center text-4xl font-bold text-white">
             Welcome to the landing page of the coding intensity report card!
           </div>
         </div>
