997f52b32ec93bd7dc4a71c8dd53501cb71ae1d3
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2024-06-03 00:24:01 -0700

    update landing page text

--- meyers-lab/app/page.tsx
+++ meyers-lab/app/page.tsx
@@ -34,60 +34,70 @@ export default function Page() {
           </div>
         </div>
       </div>
+      <div className="mx-24 my-[10vh] min-h-[10vh] p-4 text-2xl font-bold flex flex-col justify-center items-center">
+        <div className="flex justify-center text-left w-[70%]">
+          <h1 className='mt-[2vh] mr-[3vw]'> Our purpose </h1>
+        </div>
+        <div className="mt-[3vh] min-h-[10vh] w-[70%] text-xl font-normal">
+          <h1 className="mb-[5vh]">
+            Welcome to the MediCOIN (Medicare COding INtensity) Report, a project of the Brown University School of Public Health Center for Advancing Health Policy Through Research. The objective of this website is to better understand variation in measured coding intensity across Medicare Advantage contracts. We report several validated measures of coding intensity in the Medicare Advantage program to better understand variation that could lead to variable payments in the program. Our goal is to make this data publicly available for researchers and policymakers.
+          </h1>
+        </div>
+        <div className="flex justify-center text-left w-[70%]">
+          <h1 className='mt-[2vh] mr-[2vw]'> Instructions & Support </h1>
+        </div>
+        <div className="mt-[3vh] min-h-[10vh] w-[70%] text-xl font-normal">
+          <h1>
+              Anyone may download the data we calculate at the contract level on the downloads page. For detailed methods on how we calculate these measures, please see the methods page.             Please cite this website if you use these data in your own work. This project was supported by a grant from Arnold Ventures.
+          </h1>
+        </div>
+      </div>
+      {/* <div className="mx-24 pb-[10vh] min-h-[10vh] p-4 text-2xl font-bold flex flex-col justify-center items-center bg-gray-200">
+        <div className="flex justify-start text-left w-[70%]">
+          <h1 className='mt-[2vh]'> Instructions & Support </h1>
+        </div>
+        <div className="mt-[3vh] min-h-[10vh] w-[70%] text-xl font-normal">
+          <h1>
+              Anyone may download the data we calculate at the contract level on the downloads page. For detailed methods on how we calculate these measures, please see the methods page.             Please cite this website if you use these data in your own work. This project was supported by a grant from Arnold Ventures.
+          </h1>
+        </div>
+      </div> */}
 
-      {/* Purpose section */}
+
+
+      {/* Purpose section
       <div className="mx-24 my-[10vh] min-h-[10vh] p-4 text-2xl font-bold">
         <h1> Our purpose </h1>
         <div className="mt-[5vh] min-h-[10vh] w-1/2 text-xl font-normal">
           <h1>
-            The overall objective of this project is to better understand how
-            the Medicare Advantage (MA) program impacts spending through
-            increasing coding intensity. In our prior work, we have found
-            substantial MA plan level variation in the use of different tools
-            for maximizing coding intensity. Our goal for this report card is to
-            use several measures of coding intensity in MA and to publicly
-            report those measures to potentially increase pressure on plans that
-            engage in more intensive coding behavior.
+            Welcome to the MediCOIN (Medicare COding INtensity) Report, a project of the Brown University School of Public Health Center for Advancing Health Policy Through Research. The objective of this website is to better understand variation in measured coding intensity across Medicare Advantage contracts. We report several validated measures of coding intensity in the Medicare Advantage program to better understand variation that could lead to variable payments in the program. Our goal is to make this data publicly available for researchers and policymakers.
           </h1>
         </div>
-      </div>
+      </div> */}
 
-      {/* Mission statement section */}
+      {/* Mission statement section
       <div className="my-[5vh] flex min-h-[10vh] w-screen justify-end bg-gray-200 p-4">
         <div className="mx-24 my-[5vh] w-1/2 text-right text-2xl font-bold">
-          <h1> Mission Statement </h1>
+          <h1> Instructions </h1>
           <div className="my-[5vh] min-h-[10vh] text-xl font-normal">
             <h1>
-              This report card website will help to inform policymakers about
-              variations in MA coding intensity. The related policy implications
-              include first, that it will enable us to measure an unbiased
-              estimate of increased coding by using functional impairment
-              recorded on clinical assessments as a gold standard for patient
-              morbidity, allowing us to estimate how much additional spending
-              may be attributable to upcoding. Second, it will enable us to
-              determine the role that chart reviews and health risk assessments
-              play in upcoding over time, and may help to inform policy as to
-              whether these MA plan procedures should be permitted by CMS.
-              Third, it will help us to better understand and identify the plans
-              that engage in the most upcoding, which may allow for better
-              targeting of regulation to reduce the upcoding of diagnoses. This
-              has the potential to help identify billions of dollars of waste in
-              the Medicare program and will help present opportunities to
-              address it.
+              Anyone may download the data we calculate at the contract level on the downloads page. For detailed methods on how we calculate these measures, please see the methods page
             </h1>
           </div>
         </div>
-      </div>
+      </div> */}
 
-      {/* Instructions section */}
+      {/* Instructions section
       <div className="mx-24 my-[5vh] min-h-[10vh] p-4  w-1/2 text-2xl font-bold">
-        <h1> Instructions </h1>
+        <h1> Support </h1>
         <div className="my-[5vh] min-h-[10vh] text-xl font-normal">
-          <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h1>
+          <h1>
+            Please cite this website if you use these data in your own work. This project was supported by a grant from Arnold Ventures.
+          </h1>
         </div>
-      </div>
+      </div> */}
 
-      {/* Methodology section */}
+      {/* Methodology section
       <div className=" mt-[5vh] flex min-h-[10vh] w-screen justify-end bg-gray-200 p-4">
         <div className="mx-24 my-[5vh] w-1/2 text-right text-2xl font-bold">
           <h1> Methodology</h1>
@@ -107,7 +117,8 @@ export default function Page() {
             </h1>
           </div>
         </div>
-      </div>
+      </div> */}
+      
     </main>
   );
 }
